7637bce57395ed6a446b2ecd127392fc5bf22517
WANZARGEN <wanjin911003@gmail.com>
2018-09-20 18:20:16 +0900

    transition added

---
+++ transition/client/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Original</router-link> |
+      <router-link to="/about">Custom</router-link>
+    </div>
+    <h1>VUE TRANSITION</h1>
+      <router-view class="router-view"/>
+    <h2>by WANZARGEN</h2>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+h1 {
+  margin-bottom: 80px;
+}
+h2 {
+  margin-top: 40px;
+}
+.router-view {
+  width: 100%;
+  min-height: 60vh;
+}
+</style>
---
+++ transition/client/src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+    
+    <div class="text-center">
+      <img class="logo" src="../assets/logo.png">
+      <img class="logo" src="../assets/logo.png">
+      <img class="logo" src="../assets/logo.png">
+    </div>
+
+    <div class="separator"></div>
+
+    <div class="text-center">
+      <div class="img-container"><img class="logo" src="../assets/logo.png"></div>
+      <div class="img-container"><img class="logo" src="../assets/logo.png"></div>
+      <div class="img-container"><img class="logo" src="../assets/logo.png"></div>
+    </div>
+
+
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.text-center {
+  text-align: center;
+}
+img {
+  width: 100px;
+  margin: 20px;
+}
+.separator {
+  border-bottom: 1px solid gray;
+}
+.img-container {
+  img {
+    filter: sepia();
+  }
+}
+
+</style>
